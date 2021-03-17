d261b65ab9ec93f9c5e9d6a4adaf9c7ad1a0e0d2
Daniel Robertson <danielrobertson733@gmail.com>
2021-03-16 23:21:28 -0500

    flattened sights lists

--- components/SightsList.js
+++ components/SightsList.js
@@ -23,20 +23,23 @@ export default function SightsList({ list }) {
   }
 
   return (
-    <div className="border-gray-200 border-1 bg-gray-50 shadow-md mx-5 rounded-lg mb-5">
+    <div className="border-gray-200 border-1 bg-purple-400 shadow-md mx-5 rounded-lg mb-5">
       <h3 className="text-2xl pl-5 py-3 text-gray-800">{name}</h3>
       {!loading &&
-        data.sights.map((s) => (
-          <div className="pl-5 p-3 flex bg-gray-100 rounded-b-lg">
-            <img
-              src={s.image}
-              alt="sight image"
-              className="rounded-full h-10 w-10"
-            />
-            <div className="text-lg flex items-center pl-2 text-gray-800">
-              {s.name}
+        data.sights.map((sight, idx) => (
+          <>
+            {idx !== 0 && <hr className="border-grey-800 px-lg" />}
+            <div className="pl-5 p-3 flex bg-gray-100">
+              <img
+                src={sight.image}
+                alt="sight image"
+                className="rounded-full h-10 w-10"
+              />
+              <div className="text-lg flex items-center pl-2 text-gray-800">
+                {sight.name}
+              </div>
             </div>
-          </div>
+          </>
         ))}
     </div>
   );
